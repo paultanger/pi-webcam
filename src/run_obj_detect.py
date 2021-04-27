b2aabf77355381c22549f6d5010e6f2d91bf5079
Paul Tanger <detroitstylz@gmail.com>
2021-04-26 22:00:07 -0600

    work on obj detect script

--- src/run_obj_detect.py
+++ src/run_obj_detect.py
@@ -37,62 +37,64 @@ def main():
     text_time = current_time - mins_30
     # setup morning time range
     start = datetime.datetime.now().replace(hour=6, minute=30, second=0, microsecond=0)
-    end = datetime.datetime.now().replace(hour=21, minute=30, second=0, microsecond=0)
+    end = datetime.datetime.now().replace(hour=12, minute=30, second=0, microsecond=0)
 
     while True:
-        # while we are still checking frames
-        #TODO: while 
-        # setup current time
-        current_time = datetime.datetime.now().replace(second=0, microsecond=0)
-        # if haven't texted lately..
-        time_diff = (datetime.datetime.now() - text_time).total_seconds()
-        min_diff = divmod(time_diff, 60)[0]
-        # if time in range, check if camera available
-        if current_time <= end and current_time >= start and time_diff >= 30:
-            # if it is, run obj detect
-            rval, frame = vc.read() 
-            bbox, label, conf = cv.detect_common_objects(frame, confidence=.3, model='yolov4-tiny')
-            if label != []:
-                labels = [lab for lab in label if lab in egg_labels]
-                # only keep labels in this group
-                label_count = Counter(labels)
-                if len(label_count) > 1:
-                # # determine if I have been texted in the last 30 mins?
-                # time_diff = (datetime.now() - text_time).total_seconds()
-                # min_diff = divmod(time_diff, 60)[0]
-                # if min_diff >= 30:
-                    # text me..
-                    message = client.messages \
-                    .create(
-                        body = "a possible egg!",
-                        from_= twilio_phone,
-                        to = my_phone
-                    )
-                    # text Levitie
-                    # message = client.messages \
-                    # .create(
-                    #     body = "a possible egg!",
-                    #     from_= twilio_phone,
-                    #     to = lev_phone
-                    # )
-                    # text Dorothy
-                    # message = client.messages \
-                    # .create(
-                    #     body = "testing hen cam",
-                    #     from_= twilio_phone,
-                    #     to = dor_phone
-                    # )
-                    # restart time to wait 30 mins before doing again
-                    text_time = datetime.datetime.now().replace(second=0, microsecond=0)
-            # if we didn't trigger anything, check 10 more frames,
-                # TODO...
-            # then wait 5 mins before checking again
-            print('checked 10 frame, waiting 5 mins..')
-            # for testing:
-            time.sleep(3)
-            #time.sleep(300)
-
+        # while we are still checking frames.. check 10
+        for i in range(10):
+            # setup current time
+            current_time = datetime.datetime.now().replace(second=0, microsecond=0)
+            # if haven't texted lately..
+            time_diff = (datetime.datetime.now() - text_time).total_seconds()
+            min_diff = divmod(time_diff, 60)[0]
+            # if time in range, check if camera available
+            vc = cv2.VideoCapture(0)
+            if current_time <= end and current_time >= start and time_diff >= 30 and vc.isOpened() == True:
+                print('in if stmt to run obj detect')
+                # if it is, run obj detect
+                rval, frame = vc.read() 
+                bbox, label, conf = cv.detect_common_objects(frame, confidence=.3, model='yolov4-tiny')
+                if label != []:
+                    labels = [lab for lab in label if lab in egg_labels]
+                    # only keep labels in this group
+                    label_count = Counter(labels)
+                    if len(label_count) > 1:
+                        # text me..
+                        message = client.messages \
+                        .create(
+                            body = "a possible egg!",
+                            from_= twilio_phone,
+                            to = my_phone
+                        )
+                        # text Levitie
+                        # message = client.messages \
+                        # .create(
+                        #     body = "a possible egg!",
+                        #     from_= twilio_phone,
+                        #     to = lev_phone
+                        # )
+                        # text Dorothy
+                        # message = client.messages \
+                        # .create(
+                        #     body = "testing hen cam",
+                        #     from_= twilio_phone,
+                        #     to = dor_phone
+                        # )
+                        # restart time to wait 30 mins before doing again
+                        text_time = datetime.datetime.now().replace(second=0, microsecond=0)
+                # if we didn't trigger anything, check 10 more frames...
+                # close our attempt at vc
+                vc.release()
+            else:
+                print('time not in range, or texted recently or vc not available')
+        # then wait 5 mins before checking again
+        print('checked 10 frames, waiting 5 mins..')
+        # for testing:
+        #time.sleep(3)
+        time.sleep(300)
 
 
 if __name__ == '__main__':
+    # just run this indefinately..
+    # python run_obj_detect.py
     main()
